0e822594ace5b7a655d2c09072a8284800fd37b4
duzhengjun <870426133@qq.com>
2020-06-14 10:27:14 +0800

    测试

--- src/test/java/logTest.java
+++ src/test/java/logTest.java
@@ -1,5 +1,11 @@
 import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Set;
 
 public class logTest {
     static Logger logger = Logger.getLogger(logTest.class);
@@ -7,6 +13,7 @@ public class logTest {
         logger.debug("你好");
         logger.info("大家好");
         logger.error("我好");
+
 
     }
 
